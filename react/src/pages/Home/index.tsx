2d120d5ee3a96bac01927783700fd405476aa47b
ferreirazdev <business.flavioferreira@gmail.com>
2023-03-29 06:49:13 -0300

    feat: describe portifolio

--- react/src/pages/Home/index.tsx
+++ react/src/pages/Home/index.tsx
@@ -1,7 +1,23 @@
+import styled from "styled-components";
+
+import { Navbar } from "../../components/Navbar";
+import { Resume } from "../../components/Resume";
+
 export function Home() {
     return (
         <div>
-            salve
+            <Banner>
+                <Navbar />
+                <Resume />
+            </Banner>
         </div>
     )
 }
+
+const Banner = styled.div`
+  height: 100vh;
+  @media (max-width: 640px) {
+    height: 100%;
+    padding-bottom: 2rem;
+  }
+`;
